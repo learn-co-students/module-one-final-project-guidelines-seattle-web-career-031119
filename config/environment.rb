199daf0caf7ab4c0f2a9734847d9558c4293bbbb
Rylan Bauermeister <apple@MacBook-Pro.local>
2019-03-26 15:04:04 -0700

    no more logging messages

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,10 @@
 require 'bundler'
 Bundler.require
-
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/food_project.db')
 require_all 'lib'
 require_all 'app'
 require_relative "../demofile.rb"
 require_relative "../api_key.rb"
+
+
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/food_project.db')
+ActiveRecord::Base.logger.level = Logger::INFO
