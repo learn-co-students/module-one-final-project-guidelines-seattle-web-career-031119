77204fc5b393efd34d9d2f8f4254875e1fadb84c
quinncidences <qrc.inpa@gmail.com>
2019-03-26 10:46:25 -0700

    adding in recipe info

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,3 +5,45 @@ class Recipe < ActiveRecord::Base
   has_many :users, through: :meals
 
 end
+
+response = Unirest.get "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/479101/information",
+  headers:{
+    "X-RapidAPI-Key" => "XXXXXXXXXXXXXXXXXXXX"
+  }
+
+
+
+
+  def self.create_from_data(data)
+    self.create({
+      vegetarian: data["vegetarian"],
+      vegan: data["vegan"],
+      gluten_free: data["glutenFree"],
+      dairy_free: data["dairyFree"],
+      readyInMinutes: data["readyInMinutes"],
+      title: data["title"],
+      external_id: data["id"],
+      source_url: data["sourceURL"],
+      credit_text: data["creditText"]
+    })
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  #000
