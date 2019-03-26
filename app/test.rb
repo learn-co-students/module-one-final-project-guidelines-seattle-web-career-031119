5c02e2b7bf781140f2065d56f9148d15be862d81
256hz <a.d@256hz.com>
2019-03-26 10:46:05 -0700

    Beginning to lay out the CLI, added some stuff to UserStories

--- app/test.rb
+++ app/test.rb
@@ -7,6 +7,7 @@ def api_key
 end
 
 def categories
+  # gets back the zomato 'meal type' categories - lunch, dinner, delivery, etc.
   response_string = RestClient::Request.execute(method: :get,
                                     url: "https://developers\.zomato\.com/api/v2\.1/categories",
                                 headers:
@@ -16,11 +17,43 @@ def categories
 end
 
 def cities(city)
+  # gets back city info from a the string you enter.
   response_string = RestClient::Request.execute(method: :get,
                                     url: "https://developers\.zomato\.com/api/v2\.1/cities?q=#{city}",
                                 headers:
-                                  {"user-key": api_key,
-                                   "cities": city},
+                                  {"user-key": api_key},
                                 timeout: 10)
   JSON.parse(response_string)
+end
+
+def establishments(city_id)
+  # gets back the types of establishments in the provided city ID number.
+  response_string = RestClient::Request.execute(method: :get,
+                                    url: "https://developers.zomato.com/api/v2.1/establishments?city_id=#{city_id}",
+                                headers:
+                                  {"user-key": api_key},
+                                timeout: 10)
+  JSON.parse(response_string)
+end
+
+def search_city_by_cuisine(entity_id, entity_type="zone", cuisine_id)
+  # gets back all types of shit depending on what you enter.
+  response_string = RestClient::Request.execute(method: :get,
+                                    url: "https://developers\.zomato\.com/api/v2\.1/search?entity_id=#{entity_id}&entity_type=#{entity_type}&cuisines=#{cuisine_id}",
+                                headers:
+                                  {"user-key": api_key},
+                                timeout: 10)
+  search_results = JSON.parse(response_string)
+  # To return the restaurant names from your search:
+  # search_results["restaurants"].map{|rest| puts rest["restaurant"]["name"]}
+end
+
+def search_location_for_cuisines(entity_id, entity_type="subzone")
+  # gets back all types of shit depending on what you enter.
+  response_string = RestClient::Request.execute(method: :get,
+                                    url: "https://developers\.zomato\.com/api/v2\.1/search?entity_id=#{entity_id}&entity_type=#{entity_type}&cuisines=#{cuisine_id}",
+                                headers:
+                                  {"user-key": api_key},
+                                timeout: 10)
+  search_results = JSON.parse(response_string)
 end
