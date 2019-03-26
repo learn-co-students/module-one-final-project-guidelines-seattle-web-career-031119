0b58218bb50b62dfd04e454e4176df03ccb4c43a
github username <github email address>
2019-03-26 10:06:43 -0700

    environment setup

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,11 @@
 require 'bundler'
+# require_relative '../app/models/game.rb'
+# require_relative '../app/models/round.rb'
+# require_relative '../app/models/player.rb'
 Bundler.require
 
+require 'active_record'
+require 'rake'
+
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
-require_all 'lib'
+#require_all '../lib'
