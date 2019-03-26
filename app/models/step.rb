f4bfd54d39cc54cec2866548ebbdcc5fed72b4f2
Rylan Bauermeister <apple@MacBook-Pro.local>
2019-03-26 10:27:46 -0700

    added creator for step

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,3 +1,15 @@
 class Step < ActiveRecord::Base
+  belongs_to :recipe
+
+  def self.create_from_data(data, recipe_id)
+    step = self.create({
+        recipe_id: recipe_id,
+        number: data["number"],
+        step: data["step"]
+      })
+      if !data["length"].nil?
+        step.update(length: "#{data["length"]["number"]} #{data["length"]["unit"]}.")
+      end
+  end
 
 end
