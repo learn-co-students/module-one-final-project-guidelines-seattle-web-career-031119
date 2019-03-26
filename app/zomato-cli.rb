7cddaf0562ec215eb8ae6b08859aadf5b8c3c934
github username <github email address>
2019-03-26 15:21:08 -0700

    location menu cli and reviews

--- app/zomato-cli.rb
+++ app/zomato-cli.rb
@@ -1,13 +1,82 @@
-#Welcome
 
-#Login
 
-#Search location
+def menu
+  status = 0
+  while status == 0
+    puts
+    puts "Welcome! Enter 'eat' to begin searching for delicous food around Seattle or 'quit' to logout and quit the program."
+    puts
+    command = gets.chomp
+    puts
+    if command.downcase == 'eat'
+      status = 1
+      user_entry
+    elsif command.downcase == 'quit'
+      puts 'Ok, see you later.'
+      status = 1
+    else
+      puts "I'm not smart enough to understand your input, so I will return you to the menu :) Thank me later."
+    end
+  end
+end
 
-#Enter cuisine
+def user_entry
+  puts
+  puts "Please login by entering your first name:"
+  puts
+  name = gets.chomp
+  while (name.match(/^[[:alpha:]]+$/) == nil)
+    puts
+    puts 'Please enter a user name without special characters!'
+    puts
+    name = gets.chomp
+  end
+  user = User.find_or_create_by(name: name)
+  puts
+  puts "You are now logged in as #{user.name}"
+  puts
+  user_menu(user)
+end
 
-#See list of restaurants
+def user_menu(user)
+  status = 0
+  while status == 0
+  puts "#{user.name}, what would you like to do? Enter 'See Reviews', 'Search' or 'Quit' to logout."
+  puts
+  response_string = gets.chomp
+  if response_string.downcase == 'see reviews'
+    user.pretty_reviews
+    puts
+  elsif response_string.downcase == 'search'
+    puts "Enter a neighborhood or city name"
+    puts
+    location = gets.chomp
+    puts
+    hash = handle_location(location)
+    display_pretty_hash(hash)
 
-#Choose to add restaurant to list
+  elsif response_string.downcase == 'quit'
+    status = 1
+    menu
+  else
+    puts "I don't know what that means"
+    puts
+  end
+  end
+end
 
-#Go back to menu
+
+def display_pretty_hash(hash)
+  hash.each do |key, value|
+    print "#{key}: "
+    vale.each {|key, value| puts key}
+  end
+  puts
+end
+
+
+def handle_location(location)
+  array_of_options = match_input_to_location(location)
+  suggestions_menu = location_suggestions(array_of_options)
+  pretty_hash = pretty_location_menu(suggestions_menu,array_of_options)
+end
