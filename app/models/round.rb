60cc051394c033744ab8097f1e8cc786fc08d673
github username <github email address>
2019-03-27 11:08:15 -0700

    functional bare-bones cli

--- app/models/round.rb
+++ app/models/round.rb
@@ -2,17 +2,3 @@ class Round < ActiveRecord::Base
   belongs_to :games
   belongs_to :players
 end
-
-
-# # --------------put this code somewhere--------------------
-# def get_lyric
-#   lyrics = RestClient.get(self)
-#   lines = JSON.parse(lyrics)["lyrics"].split("\n").reject!(&:empty?)
-#   freq = lines.inject(Hash.new(0)) {|h,v| h[v] += 1; h}
-#   lines.max_by {|v| freq[v]}
-# end
-#
-#
-# def lyric_getter(artist, title)
-#   "https://api.lyrics.ovh/v1/#{artist.gsub(' ','%20')}/#{title.gsub(' ','%20')}".get_lyric
-# end
--- bin/run.rb
+++ bin/run.rb
@@ -3,6 +3,3 @@ require_relative '../config/environment'
 Cli.welcome_message
 Cli.get_username
 Cli.start_a_game
-
-
-puts "HELLO WORLD"
